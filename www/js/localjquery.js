7dbb9feba315b9e8c8f4cd30c0ebb6a3537b0d2f
gitlet <11thdim@gmail.com>
2016-06-01 18:52:29 +0400

    first upload

---
+++ www/js/localjquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  // alert('hi');
+
+  // $('ion-pane .login #login-btn').click(function () {
+  //   switch (this.#login-btn) {
+  //     case login: window.location.href = "../templates/homepg.html";
+  //     break;
+  //     // default:
+  //   }
+  // });
+
+  // $('#left-btn').click(function() {
+  //   $('.left-menu').slideToggle();
+  // });
+});
